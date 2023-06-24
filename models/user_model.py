54d642438403877d9043387ac71c753f7fcf6250
habyaad <adeomotoshan@gmail.com>
2023-06-24 11:25:40 +0100

    feat: add public id field and remove update user method

--- models/user_model.py
+++ models/user_model.py
@@ -1,20 +1,16 @@
 class User():
-    def __init__(self, id, first_name, email, password) -> None:
+    def __init__(self, id, first_name, email, password, public_id) -> None:
         self.id = id
         self.first_name = first_name
         self.email = email
         self.password = password
-
-    def updateUser(self, new_id, new_first_name, new_email, new_password):
-        self.id = new_id
-        self.first_name = new_first_name
-        self.email = new_email
-        self.password = new_password
+        self.public_id = public_id
 
     def serialize(self):
         return {
             "id": self.id,
             "name": self.first_name,
             "email": self.email,
-            "password": self.password
+            "password": self.password,
+            "public_id": self.public_id
         }
